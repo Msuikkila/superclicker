8572349fe1b1cba32ba10ce12550f851040af66e
Mika Suikkila <mikasuikkila@gmail.com>
2023-07-09 00:30:52 -0400

    initial commit

---
+++ src/counter.ts
@@ -0,0 +1,44 @@
+let doubled: boolean = false;
+let counter: number = 0;
+
+function incrementCounter(upgradeButton: HTMLButtonElement) {
+  const increment = doubled ? 2 : 1;
+  counter = counter + increment;
+
+  if (counter >= 5 && doubled === false) {
+    // enable double clicker
+    upgradeButton.disabled = false
+  }
+}
+
+function updateCounter(counterButton: HTMLButtonElement) {
+  counterButton.innerHTML = `Count is ${counter}`
+}
+
+
+export function setupCounter(counterButton: HTMLButtonElement, upgradeButton: HTMLButtonElement) {
+  updateCounter(counterButton)
+
+  counterButton.addEventListener('click', () => {
+    incrementCounter(upgradeButton);
+    updateCounter(counterButton)
+  })
+}
+
+export function setupUpgrade(upgradeButton: HTMLButtonElement, counterButton: HTMLButtonElement) {
+  // setting up the upgrade button
+  upgradeButton.innerHTML = "Double clicker!"
+  upgradeButton.disabled = true
+  
+  // setting up what happens when we click it
+  upgradeButton.addEventListener('click', () => {
+    if(counter >= 5) {
+    doubled = true
+    counter = counter - 5
+    updateCounter(counterButton)
+    upgradeButton.disabled = true
+    } 
+
+  })
+}
+
---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import './style.css'
+import { setupCounter, setupUpgrade } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Superclicker</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+      <button id="upgrade" type="button"></button>
+    </div>
+    <p class="docs">
+      It's an idle clicker, you know what to do
+    </p>
+  </div>
+`
+
+const counterButton = document.querySelector<HTMLButtonElement>('#counter')!
+const upgradeButton = document.querySelector<HTMLButtonElement>('#upgrade')!
+
+setupCounter(counterButton, upgradeButton)
+setupUpgrade(upgradeButton, counterButton)
